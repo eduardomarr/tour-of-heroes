65ca6d549a782e2a78beefc25a24aaf51669b9a2
eduardomarr <dado.nep@gmail.com>
2022-11-16 10:50:03 -0300

    feat: heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { heroes } from './mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
   hero: Hero = {
     id: 1,
     name: 'Naruto',
   };
 
-  constructor() {}
+  heroes = heroes;
 
-  ngOnInit(): void {}
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from '../hero.model';
+
+export const heroes: Hero[] = [
+  { id: 1, name: 'Frederich' },
+  { id: 2, name: 'Ives' },
+  { id: 3, name: 'Skipton' },
+  { id: 4, name: 'Lockwood' },
+  { id: 5, name: 'Hakim' },
+  { id: 6, name: 'Edgard' },
+  { id: 7, name: 'Lek' },
+  { id: 8, name: 'Waldemar' },
+  { id: 9, name: 'Alexio' },
+  { id: 10, name: 'Donn' },
+  { id: 11, name: 'Meredith' },
+  { id: 12, name: 'Dur' },
+  { id: 13, name: 'Gunter' },
+  { id: 14, name: 'Raynard' },
+  { id: 15, name: 'Haskel' },
+  { id: 16, name: 'Rollo' },
+  { id: 17, name: 'Drew' },
+  { id: 18, name: 'Kelbee' },
+  { id: 19, name: 'Eb' },
+  { id: 20, name: 'Stefan' },
+];
